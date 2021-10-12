aa60b0c37f7933c94c158786da500c8818d7c946
ayoubzaabali <78790570+ayoubzaabali@users.noreply.github.com>
2021-10-12 19:46:41 +0100

    setups

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,55 @@ class HomeController extends Controller
     {
         return view('home');
     }
+    /**
+     *optimize the app.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function optimize(){
+        Artisan::call('optimize');
+        Artisan::call('route:cache');
+        Artisan::call('config:cache');
+        return "App optimized";
+
+    }
+    /**
+     * Show the application login form.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function showloginform()
+    {
+        return view('login');
+    }
+
+     /**
+     * send mail to user.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function mail()
+    {
+        return view('mailUsers');
+    }
+
+    /**
+     * Show the setting page.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function setting()
+    {
+        return view('setting');
+    }
+
+    /**
+     * Search.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function search()
+    {
+        return view('search');
+    }
 }
