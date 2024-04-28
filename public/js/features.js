1fd2b653c1dbee87eb5cb1571377644509d4c242
ayoub <ayoub.zaabali@gmail.com>
2024-04-28 23:02:54 +0100

    public js

---
+++ public/js/features.js
@@ -0,0 +1,169 @@
+window.onload = async() => {
+
+    // phone click
+    let icons = document.querySelectorAll("div.navit svg");
+    for (let i = 0; i < icons.length; i++) {
+        icons[i].onclick = (evt) => {
+            let elem = evt.target.closest("svg").nextElementSibling.firstElementChild
+            elem.click()
+        }
+    }
+    let side = document.querySelector("#sidebar");
+    if (window.pageYOffset < 100) {
+        side.classList.remove("sidevis")
+    } else {
+        side.classList.add("sidevis")
+    }
+
+
+    // testemenials automatic iteration
+    var checkboxes = document.querySelectorAll("input[type='radio']");
+    var index = 0;
+    setInterval(geitdone, 2000)
+
+    function geitdone() {
+        let item = checkboxes[index]
+        console.log(item);
+        item.checked = true;
+        index++;
+        if (index == checkboxes.length)
+            index = 0;
+    }
+    // nice scroll
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    // preloade hide
+    document.querySelector(".preloader").style.display = "none";
+
+    // add animation to top element
+    document.querySelector(".up").classList.add("upan");
+
+    // add animation reveal to all elements
+    window.addEventListener("scroll", reveal);
+
+    // images animation
+    let guys = document.querySelector(".guys")
+    let img = guys.children
+    let i = 0;
+    setInterval(() => {
+        removeDisplays(img);
+        img[i].classList.toggle("display");
+        i++;
+        if (i == img.length)
+            i = 0;
+    }, 4000);
+
+    // code here
+    // Make the DIV element draggable:
+    dragElement(document.getElementById("sidebar"));
+
+    function dragElement(elmnt) {
+        var pos1 = 0,
+            pos2 = 0,
+            pos3 = 0,
+            pos4 = 0;
+        if (document.getElementById(elmnt.id + "header")) {
+            // if present, the header is where you move the DIV from:
+            document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+        } else {
+            // otherwise, move the DIV from anywhere inside the DIV:
+            elmnt.onmousedown = dragMouseDown;
+        }
+
+        function dragMouseDown(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // get the mouse cursor position at startup:
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            document.onmouseup = closeDragElement;
+            // call a function whenever the cursor moves:
+            document.onmousemove = elementDrag;
+        }
+
+        function elementDrag(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // calculate the new cursor position:
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            // set the element's new position:
+            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        }
+
+        function closeDragElement() {
+            // stop moving when mouse button is released:
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
+    }
+
+}
+
+
+// remove displays from image slider
+function removeDisplays(img) {
+    for (let i = 0; i < img.length; i++) {
+        if (img[i].classList.contains("display"))
+            img[i].classList.remove("display");
+    }
+}
+
+// animations function
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+// Create a condition that targets viewports at least 768px wide
+const mediaQuery = window.matchMedia('(min-width: 601px)')
+
+function handleTabletChange(e) {
+    // Check if the media query is true
+    if (e.matches) {
+        // Then log the following message to the console
+        let side = document.querySelector("#sidebar");
+        window.onscroll = () => {
+            if (window.pageYOffset < 100) {
+                side.classList.remove("sidevis")
+            } else {
+                side.classList.add("sidevis")
+                let icons = document.querySelectorAll("div.navit svg");
+                for (let i = 0; i < icons.length; i++) {
+                    icons[i].onclick = (evt) => {
+                        let elem = evt.target.closest("svg").nextElementSibling.firstElementChild
+                        elem.click()
+                    }
+                }
+            }
+        }
+
+
+    }
+}
+
+// Register event listener
+mediaQuery.addListener(handleTabletChange)
+
+// Initial check
+handleTabletChange(mediaQuery);
